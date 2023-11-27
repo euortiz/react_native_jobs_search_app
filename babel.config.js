4d0da99eb10a7fcedcf1e851bb936d7e4acf8244
Gustavo <guortiz098@hotmail.com>
2023-11-27 17:13:43 +0100

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,11 @@ module.exports = function (api) {
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      ["module:react-native-dotenv", {
+        envName: "EXPO_PUBLIC_RAPID_API_KEY",
+        moduleName: "@env",
+        path: ".env",
+      }],
     ],
   };
 };
